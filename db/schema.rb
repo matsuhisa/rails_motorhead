04dcd33b9ecd80fb43f4defbd85072a1514dbcdc
matsuhisa <matsuhisa+github@gmail.com>
2015-12-30 19:37:47 +0900

    Add model prefectures

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220092725) do
+ActiveRecord::Schema.define(version: 20151230072425) do
 
   create_table "books", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.integer  "price",       limit: 4
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
 end
